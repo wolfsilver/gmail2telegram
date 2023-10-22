dbea493de816a66ffdaf90660ad6747f3b267a03
zhaopeng215 <zhaopeng215@crland.com.cn>
2023-10-22 22:29:30 +0800

    feat: 优化html格式

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+import { parse } from 'node-html-parser';
+
 // https://core.telegram.org/bots/api#html-style
-const allowedTags = ['b', 'strong', 'i', 'em', 'u', 'ins', 's', 'strike', 'del', 'a', 'code', 'pre', 'tg-spoiler', 'B', 'STRONG', 'I', 'EM', 'U', 'INS', 'S', 'STRIKE', 'DEL', 'A', 'CODE', 'PRE', 'TG-SPOILER'];
-const HTags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'th'];
+const allowedTags = ['B', 'STRONG', 'I', 'EM', 'U', 'INS', 'S', 'STRIKE', 'DEL', 'A', 'CODE', 'PRE', 'TG-SPOILER', 'b', 'strong', 'i', 'em', 'u', 'ins', 's', 'strike', 'del', 'a', 'code', 'pre', 'tg-spoiler'];
+const HTags = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'TH', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'th'];
+const INLINE_TAGS = ['SPAN', 'UL', 'OL', 'TR', 'DL', 'DT', 'TABLE', 'THEAD', 'TBODY', 'TFOOT'];
 const textRegex = /\d{4,}/;
 const styleRegex = /display\s*:\s*none\s*|visibility\s*:\s*hidden/g;
 const MAIL_LENGTH = 2000;
@@ -31,12 +34,10 @@ async function readRequestBody(request) {
 }
 
 // https://github.com/taoqf/node-html-parser
-async function convertHtmlToTelegram2(html) {
-  const HTMLParser = require('node-html-parser');
+async function convertHtmlToTelegram(html) {
   length = 0;
   contentLength = 0;
-  let now = Date.now();
-  const root = HTMLParser.parse(html, {
+  const root = parse(html, {
     lowerCaseTagName: false,  // convert tag name to lower case (hurts performance heavily)
     comment: false,            // retrieve comments (hurts performance slightly)
     voidTag: {
@@ -51,14 +52,19 @@ async function convertHtmlToTelegram2(html) {
     }
   });
 
-  now = Date.now();
-  let txt = filterHtml(root.querySelector('body') ?? root.firstChild);
+  let body = root.querySelector('body');
+  if (!body) {
+    body = root.childNodes;
+  } else {
+    body = [body];
+  }
+  let txt = body.map(item => filterHtml(item)).join(' ').trim();
   txt = txt.replace(/(\n\s*){2,}/g, '\n\n').trim();
   return txt;
 }
 
 
-function filterHtml(el) {
+function filterHtml(el, PREFIX = '') {
   if (length > MAIL_MAX_LINES || contentLength > MAIL_LENGTH) {
     return '';
   }
@@ -68,7 +74,10 @@ function filterHtml(el) {
   // 文本节点
   if (el.nodeType === 3) {
     // 将包含连续6位数字的文本使用code标签包裹
-    let content = el.trimmedText.trim();
+    let content = el.trimmedText;
+    if (!content.trim()) {
+      return '';
+    }
     length += Math.max(Math.round(content.length / 50), 1);
     contentLength += content.length;
     if (content.match(textRegex)) {
@@ -81,25 +90,32 @@ function filterHtml(el) {
   if (el.attributes?.style) {
     // 正则匹配style中是否包含 display: none; visibility: hidden;
     if (styleRegex.test(el.attributes.style)) {
-      return '\n';
+      return tagName === 'SPAN' ? '' : '\n';
     }
   }
 
+  if (tagName === 'BR' || tagName === 'br') {
+    return '\n';
+  }
 
   // 处理不支持的标签
   if (!allowedTags.includes(tagName)) {
     // 将 h1-h6 转换为 b
     if (HTags.includes(el.name)) {
-      return `<b>${el.childNodes.map(filterHtml).join(' ')}</b>`;
+      return `<b>${el.childNodes.map(item => filterHtml(item)).join(' ')}</b>`;
     }
     // style, script 标签去除
     if (['style', 'script', 'iframe', 'STYLE', 'SCRIPT', 'IFRAME'].includes(el.name)) {
       return '';
     }
+    let prefix = INLINE_TAGS.includes(tagName) ? '' : PREFIX.includes('\n') ? '' : `\n`;
+    if (tagName === 'LI' || tagName === 'li') {
+      prefix += `•  `;
+    }
 
     const children = el.childNodes;
     if (children.length) {
-      return children.map(filterHtml).join(' ')
+      return prefix + children.map(item => filterHtml(item)).join('');
     }
     return '\n';
   }
@@ -114,10 +130,7 @@ function filterHtml(el) {
   }
 
   // 处理支持的标签子节点
-  if (tagName === 'BR' || tagName === 'br') {
-    return '\n';
-  }
-  return `<${tagName}>${el.childNodes.map(filterHtml).join(' ')}</${tagName}>`;
+  return `<${tagName}>${el.childNodes.map(item => filterHtml(item)).join(' ')}</${tagName}>`;
 }
 
 export default {
@@ -125,7 +138,7 @@ export default {
     if (request.method === "POST") {
       const body = await readRequestBody(request);
       if (body) {
-        const res = await convertHtmlToTelegram2(body)
+        const res = await convertHtmlToTelegram(body)
         return new Response(res);
       }
     }
